0c779e6cfe1341b93de1b51dc56129af4ae4209a
xumengqi <266347978@qq.com>
2021-03-18 22:57:38 +0800

    feature: 添加发布书籍界面

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,3 +1,5 @@
+import Toast from "@vant/weapp/dist/toast/toast";
+
 // pages/home/home.js
 Page({
 
@@ -5,7 +7,56 @@ Page({
    * 页面的初始数据
    */
   data: {
+    uploadBookShow: false,
+    uploadBookActionList: [
+      { name: "扫描ISBN", id: 0 }, { name: "手动录入", id: 1 }
+    ]
+  },
 
+  openUploadBookAction() {
+    this.setData(
+      { uploadBookShow: true }
+    )
+  },
+
+  /**
+   * 点击关闭上传书籍弹框
+   */
+  uploadBookActionOnClose() {
+    this.setData(
+      { uploadBookShow: false }
+    )
+  },
+
+  /**
+   * 选择上传书籍弹框
+   * @param {*} event 
+   */
+  uploadBookActionOnSelect(event) {
+    // 扫码上传
+    if (event.detail.id == 0) {
+      wx.scanCode({
+        success(res) {
+          if (res.scanType != "EAN_13") {
+            Toast("不是有效的ISBN，请重试或使用手动录入~");
+            return;
+          }
+          let isbn = res.result;
+          wx.navigateTo({
+            url: '/pages/upload_book/upload_book?isbn=' + isbn,
+          })
+        },
+        fail() {
+          Toast("扫描ISBN失败，请重试或使用手动录入~");
+        }
+      })
+    }
+    // 手动录入
+    else if (event.detail.id == 1) {
+      wx.navigateTo({
+        url: '/pages/upload_book/upload_book?isbn=',
+      })
+    }
   },
 
   /**
