f8d9646142310b20421d840315bccbdb36f8e0a4
xumengqi <266347978@qq.com>
2021-03-27 21:22:56 +0800

    feature: 添加书籍进度

---
+++ components/share_componet/share_record/share_record.js
@@ -0,0 +1,58 @@
+// components/share_componet/share_record/share_record.js
+var globalData = getApp().globalData;
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    borrowOrLend: {
+      type: String
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    shareRecordList: []
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    getShareRecord() {
+      let me = this
+      wx.request({
+        url: globalData.serverUrl 
+        + (me.data.borrowOrLend == "borrow" 
+        ? '/shareRecord/getShareRecordListByBorrowUserId' : '/shareRecord/getShareRecordListByShareUserId'),
+        data: {},
+        method: 'POST',
+        header: {
+          "Content-Type": "application/json",
+          'authorization': globalData.token
+        },
+        success: function (res) {
+          let dts = res.data
+          if (!dts.success) {
+            return;
+          }
+          let shareRecordList = dts.shareRecordDTOList;
+          me.setData({
+            shareRecordList: shareRecordList
+          })
+        }
+      })
+    }
+  },
+
+  lifetimes: {
+    // 生命周期函数，可以为函数，或一个在methods段中定义的方法名
+    attached: function () { 
+      this.getShareRecord()
+    },
+    moved: function () { },
+    detached: function () { },
+  },
+})
