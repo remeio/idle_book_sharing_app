fe905a74da1992110191d5b260f373edb7b1ddc0
xumengqi <266347978@qq.com>
2021-03-29 13:41:25 +0800

    feature: 添加押金显示

--- components/user/user.js
+++ components/user/user.js
@@ -1,4 +1,5 @@
 // components/user/user.js
+var globalData = getApp().globalData;
 Component({
   /**
    * 组件的属性列表
@@ -11,13 +12,43 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    userInfo: {}
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
+    getUserInfo() {
+      let me = this
+      wx.request({
+        url: globalData.serverUrl + '/user/getUserInfo',
+        data: {},
+        method: 'POST',
+        header: {
+          "Content-Type": "application/json",
+          'authorization': globalData.token
+        },
+        success: function (res) {
+          let dts = res.data
+          if (!dts.success) {
+            return;
+          }
+          let userInfo = dts.userDTO;
+          me.setData({
+            userInfo: userInfo
+          })
+        }
+      })
+    }
+  },
 
-  }
+  lifetimes: {
+    // 生命周期函数，可以为函数，或一个在methods段中定义的方法名
+    attached: function () {
+      this.getUserInfo()
+    },
+    moved: function () { },
+    detached: function () { },
+  },
 })
