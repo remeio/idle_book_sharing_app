ae811803285c611d2694ae54dbbb94a807acc8aa
xumengqi <266347978@qq.com>
2021-03-30 21:21:27 +0800

    feature: 底部图标自定义

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,6 +3,13 @@ const app = getApp()
 Page({
   data: {
     active: 0,
+    icons: [
+      { title: '首页', normal: "/resources/images/tabbar/home.png", active: "/resources/images/tabbar/home-active.png" },
+      { title: '分类', normal: "/resources/images/tabbar/catalog.png", active: "/resources/images/tabbar/catalog-active.png" },
+      { title: '书箱',  normal: "/resources/images/tabbar/bookcase.png", active: "/resources/images/tabbar/bookcase-active.png" },
+      { title: '共享', normal: "/resources/images/tabbar/share.png", active: "/resources/images/tabbar/share-active.png" },
+      { title: '我的', normal: "/resources/images/tabbar/me.png", active: "/resources/images/tabbar/me-active.png" },
+    ]
   },
   onChange(event) {
     // event.detail 的值为当前选中项的索引
@@ -12,20 +19,6 @@ Page({
  * 页面相关事件处理函数--监听用户下拉动作
  */
   onPullDownRefresh: function () {
-    wx.request({
-      url: 'http://115.29.198.114:8080/user/signIn',
-      data: {"userPhone": "11", "userPassword": "121"},
-      method: 'POST',
-      success: function(res) {
-        console.log(res.data)
-      },
-      complete: function() {
-        wx.stopPullDownRefresh({
-          success: (res) => {
 
-          },
-        })
-      }
-    })
   },
 });
