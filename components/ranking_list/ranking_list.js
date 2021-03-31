73e1718269d5ac74c2e02083a37a69534302a260
xumengqi <266347978@qq.com>
2021-04-01 00:16:49 +0800

    feature: 添加排行榜组件

---
+++ components/ranking_list/ranking_list.js
@@ -0,0 +1,28 @@
+// components/ranking_list/ranking_list.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    rankingList: {
+      type: Object
+    },
+    valueUnit: {
+      type: String
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
